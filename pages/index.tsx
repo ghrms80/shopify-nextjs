1a35723d195d039f7497b6e95f583bef3e38e797
jiho <ghrms80@gmail.com>
2022-04-12 13:53:05 +0900

    9 deploying to vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { getProductsInCollection } from "../lib/shopify";
+import ProductList from "../components/ProductList";
 
-export default function Home() {
-    return <h1 className="text-3xl">Hello from Shopify Next.js</h1>;
+export default function Home({ products }) {
+    console.log(products);
+
+    return (
+        <div className="text-3xl">
+            <ProductList products={products} />
+        </div>
+    );
 }
 
 export async function getStaticProps() {
